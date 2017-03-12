11c94084e90534abbd49a4175f9ac2183741a7d9
Ben De Meester <ben.demeester@ugent.be>
2017-03-12 21:25:04 +0100

    works with jsonld

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 var fs = require('fs'),
     path = require('path'),
-    child_process = require('child_process');
+    child_process = require('child_process'),
+    jsonld = require('jsonld');
 
 if (!fs.existsSync(path.resolve(__dirname, 'tmp'))) {
     fs.mkdirSync(path.resolve(__dirname, 'tmp'));
@@ -33,28 +34,36 @@ app.use(function (req, res, next) { // cross-origin! :D
 app.post('/_dbpedia', upload.array(), function (req, res, next) {
     var doc = req.body;
     var file = path.resolve(__dirname, 'tmp', '' + (new Date()).getTime() + '.map.ttl');
-    fs.writeFile(file, doc, 'utf8', function (err) {
+    jsonld.normalize(JSON.parse(doc), {
+        algorithm: 'URDNA2015',
+        format: 'application/nquads'
+    }, function (err, normalized) {
         if (err) {
             return close(res, file, 500, err);
         }
-        var process = child_process.spawn('java', ['-jar', path.resolve(__dirname, 'resources', 'DBpedia-Mapper-1.0-SNAPSHOT-shaded.jar'), file]);
-        var output = '';
-        process.stdout.on('data', function (data) {
-            output += data;
-        });
-        process.stderr.on('data', function (data) {
-            console.error(data.toString());
-        });
-        process.on('close', function (code) {
-            if (code !== 0) {
-                return close(res, file, 500, 'java process error!');
+        fs.writeFile(file, normalized, 'utf8', function (err) {
+            if (err) {
+                return close(res, file, 500, err);
             }
-            var tripleStart = output.indexOf('<http');
-            if (tripleStart === -1) {
-                return close(res, file, 500, 'no triples generated!');
-            }
-            output = output.slice(tripleStart).replace('{{wikititle}}', 'Belgium').trim();
-            return close(res, file, 200, output);
+            var process = child_process.spawn('java', ['-jar', path.resolve(__dirname, 'resources', 'DBpedia-Mapper-1.0-SNAPSHOT-shaded.jar'), file]);
+            var output = '';
+            process.stdout.on('data', function (data) {
+                output += data;
+            });
+            process.stderr.on('data', function (data) {
+                console.error(data.toString());
+            });
+            process.on('close', function (code) {
+                if (code !== 0) {
+                    return close(res, file, 500, 'java process error!');
+                }
+                var tripleStart = output.indexOf('<http');
+                if (tripleStart === -1) {
+                    return close(res, file, 500, 'no triples generated!');
+                }
+                output = output.slice(tripleStart).replace('{{wikititle}}', 'Belgium').trim();
+                return close(res, file, 200, output);
+            });
         });
     });
 });
